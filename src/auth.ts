922c0599f2dde516e4c2890cbc87cae63de1fef2
Artyom Leonov <6leonov@gmail.com>
2024-10-11 21:21:03 +0300

    feat: add authorized callback

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,12 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 import { db } from "@/db/db";
+
+const publicPathnames = ["/", "/terms", "/privacy"];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -13,5 +16,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   pages: {
     signIn: "/login",
+  },
+  callbacks: {
+    async authorized({ auth, request }) {
+      const pathname = request.nextUrl.pathname;
+
+      if (pathname === "/login") {
+        if (auth) {
+          const url = new URL("/", request.nextUrl.origin).toString();
+
+          return NextResponse.redirect(url);
+        }
+
+        return true;
+      }
+
+      if (!publicPathnames.includes(pathname) && !auth) {
+        return false;
+      }
+
+      return true;
+    },
   },
 });
