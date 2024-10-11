4c86ee8a8b86423fa104934583acc9f1e49e9bd7
Artyom Leonov <6leonov@gmail.com>
2024-10-11 20:47:00 +0300

    feat: add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LoginForm } from "@/features/login/login-form";
+
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { callbackUrl: string | undefined };
+}) {
+  return (
+    <main className="container max-w-screen-sm py-16">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign in to your account</CardTitle>
+
+          <CardDescription>
+            Sign in with your Google or GitHub account to continue.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <LoginForm
+            callbackUrl={searchParams.callbackUrl}
+            className="mx-auto grid gap-4 sm:grid-cols-2"
+          />
+        </CardContent>
+
+        <CardFooter>
+          <p className="w-full text-center text-sm text-muted-foreground">
+            By signing in, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="font-medium underline-offset-4 hover:underline"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="font-medium underline-offset-4 hover:underline"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
